e224211523237f764d2576611da5d086907118b4
Mark Zwart <mark.zwart@pobox.com>
2024-12-22 22:14:18 +0100

    JSON generation based on XML

---
+++ xml_to_json.py
@@ -0,0 +1,31 @@
+import json
+import pprint
+
+import xmltodict
+
+def xml_to_dict(file_xml: str) -> dict:
+    """Converting XML files describing models to Python dictionaries
+
+    Args:
+        file_xml (str): The path to a XML file
+
+    Returns:
+        dict: The data converted to a dictionary
+    """
+    # Function not yet used, but candidate for reading XML file
+    with open(file_xml) as fd:
+        doc = fd.read()
+    dict_data = xmltodict.parse(doc)
+    #dict_doc = json.dumps()
+    pprint.pprint(dict_data)
+    return dict_data
+
+def main():
+    dict_model_source = xml_to_dict("input/model_source.xml")
+    with open('input/model_source.json', 'w') as fp:
+        json.dump(dict_model_source, fp)
+    dict_model_dwh = xml_to_dict("input/model_dwh.xml")
+
+
+if __name__ == "__main__":
+    main()
